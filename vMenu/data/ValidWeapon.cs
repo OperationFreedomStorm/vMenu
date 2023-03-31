7408e24c113d5701889cea6bcb092f9853b9aac8
OperationFreedomStorm <110657276+OperationFreedomStorm@users.noreply.github.com>
2023-03-31 12:38:05 +1100

    Update ValidWeapon.cs

--- vMenu/data/ValidWeapon.cs
+++ vMenu/data/ValidWeapon.cs
@@ -819,7 +819,7 @@ namespace vMenuClient
         {
             ["Black"] = 0,
             ["Green"] = 1,
-            ["Gold"] = 2,c
+            ["Gold"] = 2,
             ["Pink"] = 3,
             ["Army"] = 4,
             ["LSPD"] = 5,
